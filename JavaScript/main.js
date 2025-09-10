75872b374e8156fc6b37c344220d3e50de19e5c0
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-09-10 17:14:30 +0500

    Spread/rest/Destructing

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,27 @@
-let x, y, z; // Statement 1
-x = 5; // Statement 2
-y = 6; // Statement 3
-z = x + y; // Statement 4
+// let x, y, z; // Statement 1
+// x = 5; // Statement 2
+// y = 6; // Statement 3
+// z = x + y; // Statement 4
 
-console.log(z);
+// console.log(z);
+
+
+// spread operator
+
+// var a = [1,2,3,4,5,6]
+// b = [...a];
+
+// console.log(b)
+ 
+// Rest operator 
+// function hello(a,b ,c, ...d){
+// console.log(a,b,c,d)
+// }
+// hello(1,2,3,4,5,6, 7)
+
+//Destructing 
+
+var a = [1 ,2 , 3]
+var [b,c,d,e] = a;
+
+console.log(b , c)
